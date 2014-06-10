0ba902d59074e087d88947bb6273ae9efb697810
frabbit <frabbit77@googlemail.com>
2014-06-10 19:46:22 +0200

    some force layout changes

--- src/js/d3/layout/Layout.hx
+++ src/js/d3/layout/Layout.hx
@@ -8,7 +8,7 @@ package js.d3.layout;
 @:native("d3.layout")
 extern class Layout {
 	/*https://github.com/mbostock/d3/wiki/Layouts*/
-	
+
 	@:overload(function(links:Array<Dynamic>):Dynamic{})
 	public function bundle():Dynamic;
 
@@ -16,23 +16,23 @@ extern class Layout {
 	public function cluster():Cluster;
 	public function force():Force;
 	public function hierarchy():Hierarchy;
-	
+
 	@:overload(function():Array<Dynamic>->Int->Histogram{})
 	@:overload(function():Array<Dynamic>->Histogram{})
 	@:overload(function():Histogram{})
 	public function histogram():Dynamic;
-	
+
 	public function pack():Pack;
 	public function partition():Partition;
-	
+
 	@:overload(function():Array<Dynamic>->Int->Array<Dynamic>{})
 	@:overload(function():Array<Dynamic>->Array<Dynamic>{})
 	public function pie():Pie;
-	
+
 	@:overload(function():Array<Dynamic>->Int->Stack{})
 	@:overload(function():Array<Dynamic>->Stack{})
 	public function stack():Stack;
-	
+
 	public function tree():Tree;
 	public function treemap():Treemap;
 }
@@ -43,19 +43,19 @@ extern class Chord implements ArrayAccess<Dynamic>{
 	/*https://github.com/mbostock/d3/wiki/Chord-Layout*/
 	@:overload(function():Array<Int>{})
 	public function matrix(matrix:Array<Int>):Chord;
-	
+
 	@:overload(function():Float{})
 	public function padding(value:Float):Chord;
-	
+
 	@:overload(function():Dynamic{})
-	public function sortGroups(comparator:Dynamic->Dynamic->Int):Chord;	
-	
+	public function sortGroups(comparator:Dynamic->Dynamic->Int):Chord;
+
 	@:overload(function():Dynamic{})
 	public function sortSubgroups(comparator:Dynamic->Dynamic->Int):Chord;
-	
+
 	@:overload(function():Dynamic{})
 	public function sortChords(comparator:Dynamic->Dynamic->Int):Chord;
-	
+
 	public function chords():Dynamic;
 	public function groups():Dynamic;
 }
@@ -63,43 +63,47 @@ extern class Chord implements ArrayAccess<Dynamic>{
 @:native("d3.layout.force")
 extern class Force implements ArrayAccess<Dynamic> {
 	/*https://github.com/mbostock/d3/wiki/Force-Layout*/
-	
+
 	@:overload(function():Array<Int>{})
-	public function size(size:Array<Int>):Force;	
-	
-	@:overload(function():Float{})	
+	public function size(size:Array<Int>):Force;
+
+	@:overload(function():Float{})
 	@:overload(function(distance:Dynamic->Float):Force{})
 	@:overload(function(distance:Void->Float):Force{})
-	public function linkDistance(distance:Float):Force;	
-	
+	public function linkDistance(distance:Float):Force;
+
 	@:overload(function():Float{})
 	@:overload(function(strength:Dynamic->Float):Force{})
 	@:overload(function(strength:Void->Float):Force{})
-	public function linkStrength(strength:Float):Force;	
-	
-	@:overload(function():Float{})	
-	public function friction(friction:Float):Force;	
-	
-	@:overload(function():Float{})	
-	public function charge(charge:Float):Force;	
-	
-	@:overload(function():Float{})	
+	public function linkStrength(strength:Float):Force;
+
+	@:overload(function():Float{})
+	public function friction(friction:Float):Force;
+
+
+	@:overload(function(node:Dynamic -> Float):Force {})
+	public function charge(charge:Float):Force;
+
+	//@:overload(function(node:Dynamic -> Float):Force {})
+	public function chargeDistance(chargeDistance:Float):Force;
+
+	@:overload(function():Float{})
 	public function theta(theta:Float):Force;
-	
-	@:overload(function():Float{})	
+
+	@:overload(function():Float{})
 	public function gravity(gravity:Float):Force;
-	
-	@:overload(function():Array<Dynamic>{})	
+
+	@:overload(function():Array<Dynamic>{})
 	public function nodes(nodes:Array<Dynamic>):Force;
-	
-	@:overload(function():Array<Dynamic>{})	
+
+	@:overload(function():Array<Dynamic>{})
 	public function links(nodes:Array<Dynamic>):Force;
-	
-	@:overload(function():Float{})	
+
+	@:overload(function():Float{})
 	public function alpha(value:Float):Force;
-	
+
 	public function on(type:String, listener:Dynamic):Force;
-	
+
 	public function drag():Void;
 	public function start():Void;
 	public function stop():Void;
@@ -109,92 +113,92 @@ extern class Force implements ArrayAccess<Dynamic> {
 
 @:native("d3.layout.pie")
 extern class Pie implements ArrayAccess<Dynamic> {
-	
+
 	/*https://github.com/mbostock/d3/wiki/Pie-Layout*/
-	@:overload(function():Dynamic{})	
+	@:overload(function():Dynamic{})
 	public function value(value:Dynamic):Pie;
-	
+
 	@:overload(function():Dynamic{})
 	public function sort(comparator:Dynamic->Dynamic->Int):Pie;
-	
-	@:overload(function():Float{})	
-	@:overload(function():Dynamic->Int->Float{})	
-	@:overload(function(value:Dynamic->Int->Float):Pie{})	
+
+	@:overload(function():Float{})
+	@:overload(function():Dynamic->Int->Float{})
+	@:overload(function(value:Dynamic->Int->Float):Pie{})
 	public function startAngle(value:Float):Pie;
-	
-	@:overload(function():Float{})	
-	@:overload(function():Dynamic->Int->Float{})	
-	@:overload(function(value:Dynamic->Int->Float):Pie{})	
+
+	@:overload(function():Float{})
+	@:overload(function():Dynamic->Int->Float{})
+	@:overload(function(value:Dynamic->Int->Float):Pie{})
 	public function endAngle(value:Float):Pie;
-	
+
 }
 
 @:native("d3.layout.stack")
 extern class Stack implements ArrayAccess<Dynamic> {
 	/*https://github.com/mbostock/d3/wiki/Stack-Layout*/
-	
+
 	@:overload(function():Dynamic{})
 	public function values(accessor:Dynamic):Stack;
-	
+
 	@:overload(function():Dynamic{})
 	@:overload(function(offset:Dynamic):Stack{})
 	public function offset(offset:String):Stack;
-	
+
 	@:overload(function():Dynamic{})
 	@:overload(function(order:Dynamic):Stack{})
 	public function order(order:String):Stack;
-	
+
 	@:overload(function():Dynamic{})
 	public function x(accessor:Dynamic->Int->Float):Stack;
-	
+
 	@:overload(function():Dynamic{})
 	public function y(accessor:Dynamic->Int->Float):Stack;
-	
+
 	@:overload(function():Dynamic->Float->Float->Void{})
-	public function out(setter:Dynamic->Float->Float->Void):Stack;	
+	public function out(setter:Dynamic->Float->Float->Void):Stack;
 }
 
 @:native("d3.layout.histogram")
 extern class Histogram implements ArrayAccess<Dynamic>{
 	/*https://github.com/mbostock/d3/wiki/Histogram-Layout*/
-	
+
 	@:overload(function():Dynamic{})
 	public function value(accessor:Dynamic):Histogram;
-	
+
 	@:overload(function():Void->Array<Float>{})
 	@:overload(function(range:Void->Array<Float>):Histogram{})
 	public function range(range:Dynamic):Histogram;
-	
+
 	@:overload(function():Dynamic{})
 	@:overload(function(bins:Dynamic):Histogram{})
 	@:overload(function(bins:Array<Float>):Histogram{})
 	public function bins(bins:Float):Histogram;
-	
+
 	@:overload(function():Bool{})
 	public function frequency(frequency:Bool):Histogram;
-	
+
 }
 
 @:native("d3.layout.hierarchy")
 extern class Hierarchy implements ArrayAccess<Dynamic> {
 	/*https://github.com/mbostock/d3/wiki/Hierarchy-Layout*/
-	
+
 	@:overload(function():Dynamic{})
 	public function sort(comparator:Dynamic->Dynamic->Int):Hierarchy;
-	
+
 	@:overload(function():Array<Int>{})
-	public function size(size:Array<Int>):Hierarchy;	
-	
+	public function size(size:Array<Int>):Hierarchy;
+
 	@:overload(function():Dynamic{})
-	public function children(accessor:Dynamic):Hierarchy;	
-	
+	public function children(accessor:Dynamic):Hierarchy;
+
 	public function nodes(root:Dynamic):Hierarchy;
-	
+
 	public function links(nodes:Array<Dynamic>):Hierarchy;
-	
-	@:overload(function():Dynamic{})	
+
+	@:overload(function():Dynamic{})
 	public function value(value:Dynamic):Hierarchy;
-	
+
 	public function revalue(root:Dynamic):Hierarchy;
 }
 
@@ -202,7 +206,7 @@ extern class Hierarchy implements ArrayAccess<Dynamic> {
 extern class Cluster extends Hierarchy {
 	/*https://github.com/mbostock/d3/wiki/Cluster-Layout*/
 	@:overload(function():Dynamic->Dynamic->Float{})
-	public function separation(separation:Dynamic->Dynamic->Float):Cluster;	
+	public function separation(separation:Dynamic->Dynamic->Float):Cluster;
 }
 
 @:native("d3.layout.pack")
@@ -224,15 +228,15 @@ extern class Tree extends Hierarchy {
 @:native("d3.layout.treemap")
 extern class Treemap extends Hierarchy {
 	/*https://github.com/mbostock/d3/wiki/Treemap-Layout*/
-	
+
 	@:overload(function():Dynamic{})
 	@:overload(function(padding:Dynamic):Treemap{})
 	@:overload(function(padding:Array<Float>):Treemap{})
 	public function padding(padding:Float):Treemap;
-	
+
 	@:overload(function():Bool{})
 	public function round(round:Bool):Treemap;
-	
+
 	@:overload(function():Bool{})
 	public function sticky(sticky:Bool):Treemap;
 }
