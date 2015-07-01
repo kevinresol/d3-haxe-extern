f137e954da86e2e8cf82ede86221663abb709c75
Ruben Weijers <ruben@vita.io>
2015-07-01 17:57:17 +0200

    change methods of `d3.Transform` to variables

--- src/js/d3/math/Transform.hx
+++ src/js/d3/math/Transform.hx
@@ -5,9 +5,9 @@ package js.d3.math;
  * @author Mike Almond - https://github.com/mikedotalmond
  */
 extern class Transform {
-	public function rotate():Float;
-	public function translate():Array<Float>;
-	public function skew():Float;
-	public function scale():Array<Float>;
+	public var rotate:Float;
+	public var translate:Array<Float>;
+	public var skew:Float;
+	public var scale:Array<Float>;
 	public function toString():String;
 }
